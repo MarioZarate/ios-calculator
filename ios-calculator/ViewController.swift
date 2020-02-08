170590ab6f71a5c5cfe4964e61f26c7c1520469c
Mario Zarate <mario.zarate@ucsp.edu.pe>
2020-02-07 22:06:35 -0500

    Initial Commit

---
+++ ios-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-calculator
+//
+//  Created by Mario Zarate Velasquez on 2/7/20.
+//  Copyright © 2020 Mario Zarate Velasquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
